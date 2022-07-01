8ae1e3149371db1da67d415633618cefbd0604b8
JONGMIN KIM <bellmin9321@gmail.com>
2022-07-02 05:20:10 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+    node: true, 
+  },
+  ignorePatterns: ["node_modules/", ".eslintrc.*", "package.json"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/react-in-jsx-scope": ["off"],
+    "prefer-arrow-callback": ["error", { allowNamedFunctions: true }],
+    "no-const-assign": ["warn"],
+    "no-dupe-else-if": ["error"],
+    "no-unused-vars": ["warn"],
+    "no-irregular-whitespace": ["error"],
+    "camelcase": ["error"],
+    "curly": ["warn"],
+    "eqeqeq": ["warn"],
+    "no-var": ["error"],
+    "prefer-const": ["warn"],
+    "prefer-destructuring": ["warn"],
+    "react/prop-types": ["off"],
+  },
+  parserOptions: {
+    parser: "babel-parser",
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  trailingComma: "all",
+  printWidth: 80,
+  arrowParens: "always",
+  endOfLine: "auto",
+};
