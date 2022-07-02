d7dd817a283730cd23e14d18c4691f94457078db
JONGMIN KIM <bellmin9321@gmail.com>
2022-07-02 16:34:22 +0900

    Add: App.jsx에 라우터 추가 및 Home 화면 구성 - 전체 배경화면 css - 사진 및 텍스트 추가 - 라우터 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,40 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import Main from "./Main";
+import AnalysisResult from "./AnalysisResult";
+import NotFound from "./NotFound";
+import Home from "./Home";
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <AppLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/result" element={<AnalysisResult />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AppLayout>
+  );
 }
+
+const AppLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70%;
+  }
+`;
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+function Home() {
+  return (
+    <>
+      <Link to="/main">
+        <Logo src="image/beHonest.png" />
+      </Link>
+      <Title>Be honest</Title>
+    </>
+  );
+}
+
+const Logo = styled.img`
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+    transform: scale(1.05);
+  }
+`;
+
+const Title = styled.div`
+  font-family: "Splash", cursive;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 120px;
+  letter-spacing: -0.03em;
+  color: #d33c0f;
+
+  :hover {
+    color: #131373;
+    transform: scale(1.05);
+  }
+`;
+
+export default Home;
