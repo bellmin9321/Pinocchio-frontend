a9e29766c50b11be5b83245c5f2dac20be64f08c
JONGMIN KIM <bellmin9321@gmail.com>
2022-07-08 15:34:16 +0900

    Feat: 로딩 컴포넌트 추가 - setTimeout을 이용해서 4초 후에 Main의 WebcamScreen이 렌더링되게 구현 - 피노키오 Gif 추가 - LOADING . . . 글자 시간에따라 생기게 애니메이션 추가

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import WebcamScreen from "./WebcamScreen";
 import Question from "./Question";
 import WebcamButton from "./WebcamButton";
+import Loading from "./Loading";
 
 function Main() {
   const [isQuestionStarted, setIsQuestionStarted] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const loadingTime = () =>
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+
+  useEffect(() => {
+    loadingTime();
+  }, []);
 
   return (
     <ContentBox>
-      <WebcamScreen isQuestionStarted={isQuestionStarted} />
-      <Question
-        isQuestionStarted={isQuestionStarted}
-        setIsQuestionStarted={setIsQuestionStarted}
-      />
-      <WebcamButton />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <WebcamScreen isQuestionStarted={isQuestionStarted} />
+          <Question
+            isQuestionStarted={isQuestionStarted}
+            setIsQuestionStarted={setIsQuestionStarted}
+          />
+          <WebcamButton />
+        </>
+      )}
     </ContentBox>
   );
 }
