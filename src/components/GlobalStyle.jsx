5e55fad83f00b43295c36f60e507d4b40c3b06db
JONGMIN KIM <49411767+bellmin9321@users.noreply.github.com>
2022-07-22 21:54:10 +0900

    Feat: 스크린샷 이미지 Carousel로 보여주기 구현 - Chore: react-image-gallery / react-speech-recognition설치 - 사용하지 않는 three.js 라이브러리 삭제

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
